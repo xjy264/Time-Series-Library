ce27cafe65d96a4defb499cbdc3153e72175709a
薛家耀 <xuejiayao@xuejiayaodeMac-mini.local>
2026-01-31 09:28:21 +0800

    chore: default to CPU/MPS when CUDA unavailable

--- run.py
+++ run.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
 
     # GPU
-    parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
+    parser.add_argument('--use_gpu', type=bool, default=False, help='use gpu')
     parser.add_argument('--gpu', type=int, default=0, help='gpu')
     parser.add_argument('--gpu_type', type=str, default='cuda', help='gpu type')  # cuda or mps
     parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
